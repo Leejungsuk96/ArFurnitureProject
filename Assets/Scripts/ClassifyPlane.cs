fad9347f1eeb4890ad6549b50aaebaaa5e9cc7ae
Leejungsuk96 <svwsv3462@gmail.com>
2024-04-04 16:26:07 +0900

    [Add]

---
+++ Assets/Scripts/ClassifyPlane.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+public class ClassifyPlane : MonoBehaviour
+{
+    public ARPlane arPlane;
+
+    public MeshRenderer planeMeshRenderer;
+
+    public TextMeshPro text;
+
+    public GameObject textObj;
+
+    GameObject mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
